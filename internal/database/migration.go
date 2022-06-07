a4e79a81d05f157ac84c2ae85ebd841ffe9dc113
Colton Bristow <cbristow4@uco.edu>
2022-06-07 00:34:58 -0500

    Add files via upload

---
+++ internal/database/migration.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"github.com/VooDooStack/FitStackAPI/internal/comment"
+	"github.com/jinzhu/gorm"
+)
+
+// MigrateDB - migrates our database and creates our comment table
+func MigrateDB(db *gorm.DB) error {
+	if result := db.AutoMigrate(&comment.Comment{}); result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
