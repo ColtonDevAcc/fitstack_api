52a70aeee99f54e36a87b053deef95940c702c85
Colton Bristow <cbristow99@gmail.com>
2022-06-29 14:07:58 -0500

    fixed firebase and cloud run

--- config/firebase.go
+++ config/firebase.go
@@ -13,20 +13,27 @@ import (
 
 func SetupFirebase() (*auth.Client, error) {
 	var opt option.ClientOption
+	var app *firebase.App
+	var err error
 
 	file := os.Getenv("FIREBASE_CREDENTIALS_FILE")
 	if len(file) == 0 {
 		fmt.Println("init firebase using default credentials file")
-		opt = nil
+
+		//Firebase admin SDK initialization
+		app, err = firebase.NewApp(context.Background(), nil)
+		if err != nil {
+			panic(fmt.Sprintf("error initializing app: %v", err))
+		}
 	} else {
 		fmt.Println("init firebase using specified credentials file")
-		opt = option.WithCredentialsFile(file)
-	}
 
-	//Firebase admin SDK initialization
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		panic(fmt.Sprintf("error initializing app: %v", err))
+		opt = option.WithCredentialsFile(file)
+		//Firebase admin SDK initialization
+		app, err = firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			panic(fmt.Sprintf("error initializing app: %v", err))
+		}
 	}
 
 	//Firebase Auth
