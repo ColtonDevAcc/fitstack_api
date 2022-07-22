e61d12e7faea06e8c38e360203673460f93c24b5
ColtonDevAcc <cbristow99@gmail.com>
2022-07-21 19:21:42 -0500

    added user routes to handler

--- api/router.go
+++ api/router.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"os"
 
-	"github.com/VooDooStack/FitStackAPI/api/handler"
 	"github.com/VooDooStack/FitStackAPI/config"
+	_userHandler "github.com/VooDooStack/FitStackAPI/data/user/delivery"
+	_userRepo "github.com/VooDooStack/FitStackAPI/data/user/repository"
+	_userUseCase "github.com/VooDooStack/FitStackAPI/data/user/usecase"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -31,7 +33,21 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	gin.SetMode(os.Getenv("GIN_MODE"))
 	r.SetTrustedProxies([]string{"192.168.1.2"})
 
-	handler.SetUpHandlers(v)
+	setUpHandlers(v, db)
 
 	return r
+}
+
+func setUpHandlers(v *gin.RouterGroup, db *gorm.DB) {
+	v.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}) //.Use(middleware.AuthMiddleware)
+
+	//===========================User===========================//
+	userRepo := _userRepo.NewUserRepository(*db)
+	userUsecase := _userUseCase.NewUserUseCase(userRepo)
+	_userHandler.NewUserHandler(v, userUsecase)
+	//===========================User===========================//
 }
--- data/user/delivery/user_handler.go
+++ data/user/delivery/user_handler.go
@@ -1,4 +1,4 @@
-package http
+package delivery
 
 import (
 	"net/http"
